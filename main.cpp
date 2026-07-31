4278100cc5a5a88a33983575c602a8b99c2c8748
rudhrapatel25 <rudhrapatel25@gmail.com>
2026-07-31 14:11:43 -0400

    Add environmental monitoring and status system

--- main.cpp
+++ main.cpp
@@ -9,6 +9,14 @@ enum LightLevel {
   BRIGHT  //(2)
 };
 
+enum EnvironmentStatus {
+  HEALTHY,
+  HOT,
+  HUMID,
+  DARK_ROOM
+};
+
+EnvironmentStatus roomStatus = HEALTHY;
 LightLevel lightStatus = NORMAL;
 
 #define SCREEN_WDH 128
@@ -27,6 +35,7 @@ float humidity = 0;
 
 #define LED_PIN 18
 
+
 void updateOLED(){
   display.clearDisplay();
   display.setTextColor(SSD1306_WHITE);
@@ -39,17 +48,17 @@ void updateOLED(){
 
   display.setTextSize(1);
 
-  display.setCursor(0,23);
+  display.setCursor(0,21);
   display.print("Temp: ");
   display.print(temp,1);
   display.println(" C");
 
-  display.setCursor(0,37);
+  display.setCursor(0,32);
   display.print("Humidity: ");
   display.print(humidity,1);
   display.print("%");
 
-  display.setCursor(0,50);
+  display.setCursor(0,43);
   display.print("Light: ");
   switch(lightStatus){
     case DARK: 
@@ -65,22 +74,60 @@ void updateOLED(){
       break;
   }
 
+  display.setCursor(0, 54);
+  display.print("Status: ");
+  switch(roomStatus){
+    case HEALTHY:
+      display.print("Healthy:)");
+      break;
+   case HOT:
+      display.print("HOT!!!");
+      break;
+    case HUMID:
+      display.print("Humid:(");
+      break;
+    case DARK_ROOM:
+      display.print("SPOOKY:/");
+      break;
+  }
   display.display();
 }
 
-void updateLED() {
+unsigned long previousLEDTime = 0;
+bool ledState = false;
 
-  switch (lightStatus){
-    case BRIGHT: 
-      digitalWrite(LED_PIN, LOW);
+void updateLED() {
+  unsigned long currentTime = millis();
+
+  switch (roomStatus){
+    case HEALTHY: 
+      digitalWrite(LED_PIN, LOW); //OFF
       break;
   
-    case NORMAL:
-      digitalWrite(LED_PIN, HIGH);
+    case HOT: 
+      digitalWrite(LED_PIN, HIGH); //ON
       break;
 
-    case DARK: 
-      digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    case HUMID: //Slow blink every 1000ms
+      if(currentTime - previousLEDTime >= 1000)
+      {
+        previousLEDTime = currentTime;
+        ledState = !ledState;
+        digitalWrite(LED_PIN, ledState);
+      }
+      break;
+
+    case DARK_ROOM: //Fast blink every 250ms
+      if(currentTime - previousLEDTime >= 250)
+      {
+        previousLEDTime = currentTime;
+        ledState = !ledState;
+        digitalWrite(LED_PIN, ledState);
+      }
+      break;
+
+    default: //OFF for safety
+      digitalWrite(LED_PIN, LOW);
       break;
   }
 }
@@ -95,12 +142,26 @@ void updateLightStatus(){
   else {
     lightStatus = BRIGHT;
   }
+}
 
+void updateEnvironmentStatus(){
+
+  if(temp > 30){
+    roomStatus = HOT;
+  }
+  else if(humidity > 70){
+    roomStatus = HUMID;
+  }
+  else if(lightStatus == DARK){
+    roomStatus = DARK_ROOM;
+  } 
+  else {
+    roomStatus = HEALTHY;
+  }
 }
 
 void setup() {
   Serial.begin(115200);
-
 
   dht.begin();
   
@@ -139,10 +200,10 @@ void loop() {
     Serial.print("Light ADC: ");
     Serial.println(lightValue);
 
+    updateEnvironmentStatus();
     updateOLED();
     updateLED();
 
   }
 
-  delay(1000);
 }
