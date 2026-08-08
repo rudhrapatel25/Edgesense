55ef232da1a499e2a763d90473e0e08f5d64d363
rudhrapatel25 <rudhrapatel25@gmail.com>
2026-08-08 10:25:35 -0400

    Implement RTOS robot state machine and OLED status

--- WarehouseBot-RTOS/src/main.cpp
+++ WarehouseBot-RTOS/src/main.cpp
@@ -3,25 +3,6 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-enum RobotState {
-  idle,
-  movingForward,
-  turningLeft,
-  turningRight,
-  stopped
-};
-
-volatile RobotState robotState = idle;
-
-#define STEP_PIN_LEFT 18
-#define DIR_PIN_LEFT 19
-
-#define STEP_PIN_RIGHT 21
-#define DIR_PIN_RIGHT 22
-
-#define TRIG_PIN 4
-#define ECHO_PIN 5
-
 #define screenwidth 128
 #define screenheight 64
 
@@ -35,186 +16,371 @@ Adafruit_SSD1306 display(
   -1
 );
 
+enum RobotState {
+  idle,
+  movingForward,
+  turningLeft,
+  turningRight,
+  stopped
+};
+
+
+volatile RobotState robotState = idle;
+
+
+#define STEP_PIN_LEFT 18
+#define DIR_PIN_LEFT 19
+
+
+#define STEP_PIN_RIGHT 21
+#define DIR_PIN_RIGHT 22
+
+
+#define TRIG_PIN 4
+#define ECHO_PIN 5
+
+
 const float safeDistance = 20.0;
+
 
 volatile float latestDistance = 999.0;
 
-bool turnLeftNext = true;
-bool obstacleHandled = false;
+
+//Helper function
+void stepMotor(int stepPin){
+  //That sequence creates one pulse; ONE pulse = one step
+  //Move ONE motor exactly one step
+  //Motor connected to this STEP pin, move one step
+  digitalWrite(stepPin, HIGH);
+  delayMicroseconds(500);
 
 
-// Changes the direction of both motors
-void setMotorDirections(bool leftDirection, bool rightDirection)
-{
+  digitalWrite(stepPin, LOW);
+  delayMicroseconds(500);
+}
+
+
+//Choose which direction each motor should rotate
+//Think of it like shifting the car into Drive or Reverse
+//In Arduino/C++, true = HIGH and false = LOW
+void setMotorDirections(bool leftDirection, bool rightDirection){
   digitalWrite(DIR_PIN_LEFT, leftDirection);
   digitalWrite(DIR_PIN_RIGHT, rightDirection);
 }
 
 
-// Sends one step pulse to both motors
-void stepBothMotorsOnce()
-{
-  digitalWrite(STEP_PIN_LEFT, HIGH);
-  digitalWrite(STEP_PIN_RIGHT, HIGH);
-
-  delayMicroseconds(500);
-
-  digitalWrite(STEP_PIN_LEFT, LOW);
-  digitalWrite(STEP_PIN_RIGHT, LOW);
-
-  delayMicroseconds(500);
+//Move both motors together for a certain number of steps
+void moveBothMotors(int steps){
+  //Both motors rotate together
+  for(int i = 0; i < steps; i++){
+    stepMotor(STEP_PIN_LEFT);
+    stepMotor(STEP_PIN_RIGHT);
+  }
 }
 
 
-// Reads the ultrasonic sensor
-float getDistance()
-{
+void moveForward(int steps){
+  setMotorDirections(HIGH, LOW);
+  moveBothMotors(steps);
+}
+
+
+void moveBackward(int steps){
+  setMotorDirections(LOW, HIGH);
+  moveBothMotors(steps);
+}
+
+
+void turnLeft(int steps){
+  setMotorDirections(LOW, LOW);
+  moveBothMotors(steps);
+}
+
+
+void turnRight(int steps){
+  setMotorDirections(HIGH, HIGH);
+  moveBothMotors(steps);
+}
+
+
+float getDistance(){
+
+
+  //Reset the trigger pin
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
 
+
+  //Send a 10 µs trigger pulse (Start measuring)
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
 
+
+  //End the trigger pulse
   digitalWrite(TRIG_PIN, LOW);
 
-  unsigned long duration = pulseIn(ECHO_PIN, HIGH, 30000);
 
-  if (duration == 0)
-  {
-    return 999.0;
-  }
+  //Wait for the echo and measure how long the ECHO pin stays HIGH (Round trip time)
+  long duration = pulseIn(ECHO_PIN, HIGH);
 
+
+  //Convert the measurted time into centimeters
   float distance = (duration * 0.0343) / 2;
+
 
   return distance;
 }
 
 
-// Returns text for the current robot state
-const char* getRobotStateName()
-{
-  switch (robotState)
-  {
-    case idle:
-      return "IDLE";
-
-    case movingForward:
-      return "FORWARD";
-
-    case turningLeft:
-      return "TURN LEFT";
-
-    case turningRight:
-      return "TURN RIGHT";
-
-    case stopped:
-      return "STOPPED";
-  }
-
-  return "UNKNOWN";
+void stopRobot(){
 }
 
+const unsigned long stopTime = 300;
+const unsigned long turnTime = 900;
 
-// Sensor task only reads distance
-void sensorTask(void* parameter)
-{
-  while (true)
-  {
+unsigned long stateStartTime = 0;
+
+bool turnLeftNext = true;
+/*This function only thinks
+//It never moves a motor
+//Each time an obstacle is detected:
+          //First obstacle = turn left
+          //Next obstacle = turn right
+          //Next obstacle = turn left
+void updateState(){
+  //float distance = getDistance();
+
+
+  //Sensor Task (Sensor keeps updating distance every 200ms
+                //Meanwhile main loop keeps deciding and controlling motors
+  //reads distance every 200 ms
+      //↓
+  //latestDistance
+      //↓
+  //Arduino loop updates state and controls motors
+
+
+  if(latestDistance < safeDistance){
+    if(turnLeftNext){
+    robotState = turningLeft;
+    }
+    else {
+      robotState = turningRight;
+    }
+    turnLeftNext = !turnLeftNext;
+    obstacleHandled = true;
+  }
+  else {
+    robotState = movingForward;
+  }
+}*/
+
+
+/*void executeState(){
+  switch(robotState){
+    case movingForward:
+      Serial.println("State: Moving forward");
+      moveForward(10);
+      break;
+
+
+    case turningLeft:
+      Serial.println("State: Turning left");
+      turnLeft(50);
+      break;
+
+
+    case turningRight:
+      Serial.println("State: Turning right");
+      stopRobot();
+      delay(200);
+      turnRight(50);
+      break;
+
+
+    case stopped:
+      Serial.println("State: Stopped");
+      stopRobot();
+      break;
+
+
+    case idle:
+      Serial.println("State: Idle");
+      stopRobot();
+      break;
+  }
+}*/
+
+
+void sensorTask(void *parameter){
+
+
+  //required FreeRTOS task format
+  while(true){
     latestDistance = getDistance();
+
 
     Serial.print("Distance: ");
     Serial.print(latestDistance);
     Serial.println(" cm");
 
+
+    //Only sensor tasks sleeps for 200ms. Other tasks run
     vTaskDelay(pdMS_TO_TICKS(200));
   }
 }
 
 
-// Navigation task only decides what the robot should do
-void navigationTask(void* parameter)
-{
-  while (true)
-  {
-    if (latestDistance < safeDistance)
-    {
-      if (!obstacleHandled)
-      {
-        if (turnLeftNext)
-        {
-          robotState = turningLeft;
-          Serial.println("State: turning left");
-        }
-        else
-        {
-          robotState = turningRight;
-          Serial.println("State: turning right");
-        }
+void stepBothMotorsOnce(){
+  digitalWrite(STEP_PIN_LEFT, HIGH);
+  digitalWrite(STEP_PIN_RIGHT, HIGH);
 
-        turnLeftNext = !turnLeftNext;
-        obstacleHandled = true;
-      }
-    }
-    else
-    {
-      robotState = movingForward;
-      obstacleHandled = false;
-    }
 
-    vTaskDelay(pdMS_TO_TICKS(100));
-  }
+  delayMicroseconds(500);
+
+
+  digitalWrite(STEP_PIN_LEFT, LOW);
+  digitalWrite(STEP_PIN_RIGHT, LOW);
+
+
+  delayMicroseconds(500);
 }
 
 
-// Motor task reads robotState and moves the motors
-void motorTask(void* parameter)
-{
-  while (true)
+//Every 20ms, check the current robot state and perform the matching
+//motor action
+void motorTask(void *parameter){
+  while(true)
   {
-    switch (robotState)
+    switch(robotState)
     {
       case movingForward:
         setMotorDirections(HIGH, LOW);
         stepBothMotorsOnce();
         break;
 
+
       case turningLeft:
         setMotorDirections(LOW, LOW);
         stepBothMotorsOnce();
         break;
+
 
       case turningRight:
         setMotorDirections(HIGH, HIGH);
         stepBothMotorsOnce();
         break;
 
+
       case stopped:
       case idle:
+        //No STEP pulse means the motors do not move
         break;
     }
+
 
     vTaskDelay(pdMS_TO_TICKS(1));
   }
 }
 
-
-// Display task shows the current robot information
-void displayTask(void* parameter)
-{
-  while (true)
+const char* getRobotStateName(){
+  switch(robotState)
   {
+    case idle:
+      return "IDLE";
+
+
+    case movingForward:
+      return "FORWARD";
+
+
+    case turningLeft:
+      return "TURN LEFT";
+
+
+    case turningRight:
+      return "TURN RIGHT";
+
+
+    case stopped:
+      return "STOPPED";
+  }
+
+
+  return "UNKNOWN";
+}
+
+void changeRobotState(RobotState newState){
+  if(robotState != newState){
+    robotState = newState;
+    stateStartTime = millis();
+
+    Serial.print("State changed: ");
+    Serial.println(getRobotStateName());
+  }
+}
+
+void navigationTask(void* parameter){
+  while(true){
+    unsigned long currentTime = millis();
+
+    switch(robotState){
+      case idle: 
+        changeRobotState(movingForward);
+        break;
+
+      case movingForward:
+        if(latestDistance < safeDistance){
+          changeRobotState(stopped);
+        }
+        break;
+
+      case stopped: 
+        if(currentTime - stateStartTime >= stopTime){
+          if(turnLeftNext){
+            changeRobotState(turningLeft);
+          }
+
+          else{
+            changeRobotState(turningRight);
+          }
+
+          turnLeftNext = !turnLeftNext;
+        }
+
+        break;
+
+        case turningLeft: 
+        case turningRight:
+          if(currentTime - stateStartTime >= turnTime){
+            changeRobotState(movingForward);
+          }
+          break;
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(50));
+  }
+}
+
+
+void displayTask(void *parameter){
+  while(true){
     display.clearDisplay();
     display.setTextColor(SSD1306_WHITE);
+
 
     display.setTextSize(2);
     display.setCursor(4, 0);
     display.println("Warehouse");
 
+
     display.setTextSize(1);
-    display.setCursor(42, 16);
-    display.println("Bot");
+    display.setCursor(42,16);
+    display.println("Bot:)");
+
 
     display.drawLine(0, 27, 127, 27, SSD1306_WHITE);
+
 
     display.setCursor(0, 33);
     display.print("Distance: ");
@@ -225,54 +391,72 @@ void displayTask(void* parameter)
     display.print("State: ");
     display.println(getRobotStateName());
 
-    display.setCursor(0, 56);
+
+    display.setCursor(0,55);
     display.print("RTOS: Running");
+
 
     display.display();
 
-    vTaskDelay(pdMS_TO_TICKS(200));
+
+    vTaskDelay(pdMS_TO_TICKS(200)); //Updates every 200ms but
+    //does not control the robot
   }
 }
 
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
+
 
   pinMode(STEP_PIN_LEFT, OUTPUT);
   pinMode(DIR_PIN_LEFT, OUTPUT);
 
+
   pinMode(STEP_PIN_RIGHT, OUTPUT);
   pinMode(DIR_PIN_RIGHT, OUTPUT);
+
+
+  digitalWrite(DIR_PIN_LEFT, HIGH);
+  digitalWrite(DIR_PIN_RIGHT, HIGH);
+
 
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
 
-  // Start the OLED before creating displayTask
+
+  //Display
   Wire.begin(oled_sda, oled_scl);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
   {
     Serial.println("OLED failed");
-
-    while (true)
-    {
-      delay(1000);
-    }
   }
 
-  display.clearDisplay();
-  display.display();
+//Sensor Task
+//reads HC-SR04
+  //   ↓
+//latestDistance
 
 
+//Navigation Task
+//reads latestDistance
+  //  ↓
+//robotState
+
+
+//Motor Task
+//reads robotState
+    //↓
+//moves motors
   xTaskCreatePinnedToCore(
-    sensorTask,
-    "Sensor Task",
-    2048,
-    NULL,
-    1,
-    NULL,
-    0
+    sensorTask,         //Function to run
+    "Sensor Task",      //Task name
+    2048,               //Stack memory
+    NULL,               //No parameters
+    1,                  //Priority
+    NULL,               //No task handle
+    0                   // Core 0
   );
 
 
@@ -287,6 +471,7 @@ void setup()
   );
 
 
+  //The motor task gets the higher priority on Core 1
   xTaskCreatePinnedToCore(
     motorTask,
     "Motor Task",
@@ -307,12 +492,7 @@ void setup()
     NULL,
     0
   );
-
-  Serial.println("WarehouseBot display task started");
 }
 
-
-void loop()
-{
-  delay(1000);
+void loop() {
 }
